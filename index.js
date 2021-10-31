eefcce47a6424ac2a82a0724309fcaf7eeaff6ee
ShimulAUST <shimulpaul59@gmail.com>
2021-10-31 11:29:07 +0600

    Update

--- index.js
+++ index.js
@@ -38,9 +38,19 @@ async function run() {
             console.log(result);
             res.send(result);
         });
+        //booking post api dynamic
+        app.get('/services/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: ObjectId(id) };
+            const service = await servicesCollection.findOne(query);
+            res.json(service);
+        })
+
     }
     finally {
         //await client.close();
+
+
     }
 }
 run().catch(console.dir);
