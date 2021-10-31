e789bbaa11ec00886060ed8962fdb08952fd3714
ShimulAUST <shimulpaul59@gmail.com>
2021-10-31 13:24:05 +0600

    delete order api

--- index.js
+++ index.js
@@ -60,6 +60,15 @@ async function run() {
             const orders = await cursor.toArray();
             res.send(orders);
         });
+        //delete api
+
+        //delete api
+        app.delete('/orders/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: ObjectId(id) };
+            const result = await servicesCollection.deleteOne(query);
+            res.json(result);
+        })
 
     }
     finally {
